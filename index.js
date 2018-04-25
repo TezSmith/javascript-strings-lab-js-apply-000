208cacc8d68bd666e5e6e94ab4186a4021a30a4c
Montez Smith <montez.smith1@gmail.com>
2018-04-25 12:23:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 var greeting = 'Hello, everybody!';
+=======
+var greeting = 'Hello, everybody';
+>>>>>>> 5532364500f2b727d65f6d932c2bb318172a04bc
 
 var specialGuest = "Neil deGrasse Tyson"
 
@@ -6,4 +10,8 @@ var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
 
+<<<<<<< HEAD
 var conversation = `Let's talk about ${topic}.`;
+=======
+var conversation = `Let's talk about ${topic}`;
+>>>>>>> 5532364500f2b727d65f6d932c2bb318172a04bc
